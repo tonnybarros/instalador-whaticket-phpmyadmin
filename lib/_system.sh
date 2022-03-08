f083fa6c87539cab93b70f08115b78b533fdf9d5
vojoobrasil <96211231+vojoobrasil@users.noreply.github.com>
2022-03-08 09:21:27 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/canove/whaticket  /home/deploy/whaticket/
+  git clone https://github.com/w3nder/whaticket  /home/deploy/whaticket/
 EOF
 
   sleep 2
