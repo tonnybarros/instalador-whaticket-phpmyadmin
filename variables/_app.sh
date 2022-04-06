61e18619aca48f0995b06cfd7c1d9555f6279aeb
vojoobrasil <96211231+vojoobrasil@users.noreply.github.com>
2022-04-06 15:34:07 -0300

    Update _app.sh

--- variables/_app.sh
+++ variables/_app.sh
@@ -13,7 +13,7 @@ mysql_root_password=$(openssl rand -base64 32)
 
 db_pass=$(openssl rand -base64 32)
 
-db_user=whaticket
-db_name=whaticket
+db_user=owenzap
+db_name=owenzap
 
-deploy_email=deploy@whaticket.com
+deploy_email=deploy@owenzap.com
