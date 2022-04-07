a463bacb688d3862b2dde61a56ff60ac48fc4d5b
owenzap <103150476+owenzap@users.noreply.github.com>
2022-04-06 23:55:45 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/canove/whaticket  /home/deploy/owenzap/
+  git clone https://github.com/whaticket/whaticket-community  /home/deploy/owenzap/
 EOF
 
   sleep 2
