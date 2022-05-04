578b21c71437cb52e32d5128ff421e7cd6b011ee
owenzap <103150476+owenzap@users.noreply.github.com>
2022-05-04 17:42:28 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -76,11 +76,13 @@ system_node_install() {
   sudo su - root <<EOF
   curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
   apt-get install -y nodejs
+  sleep 2
+  npm install -g npm@latest
+  
 EOF
 
   sleep 2
 }
-
 #######################################
 # installs docker
 # Arguments:
