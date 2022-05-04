41d2a4726193d031048f7093029e86fbc88caacb
owenzap <103150476+owenzap@users.noreply.github.com>
2022-05-04 00:42:18 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -97,9 +97,11 @@ system_docker_install() {
   apt install -y apt-transport-https \
                  ca-certificates curl \
                  software-properties-common
+
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
   
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
   apt install -y docker-ce
 EOF
 
