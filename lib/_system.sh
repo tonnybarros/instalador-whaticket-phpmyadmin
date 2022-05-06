3c02fdffd9b6c3799dc82d4ff08c2da48814d9d7
owenzap <103150476+owenzap@users.noreply.github.com>
2022-05-06 14:17:05 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -82,6 +82,8 @@ system_node_install() {
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo apt-get update -y && sudo apt-get -y install postgresql
+  sleep 2
+  sudo timedatectl set-timezone America/Sao_Paulo
   
 EOF
 
