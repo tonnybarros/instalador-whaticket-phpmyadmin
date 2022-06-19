9d463c02098c25ec5d2acc33c9e72cc694a264bc
Tonny Barros <68646516+tonnybarros@users.noreply.github.com>
2022-06-19 18:21:47 -0300

    Update _frontend.sh

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -152,12 +152,35 @@ frontend_nginx_setup() {
 
 sudo su - root << EOF
 
-cat > /etc/nginx/sites-available/${instancia_add}-frontend << 'END'
+cat > /etc/nginx/sites-available/${instancia_add}-phpmyadmin << 'END'
+server {
+  server_name $frontend_hostname/phpmyadmin;
+
+  location / {
+    proxy_pass http://127.0.0.1:${frontend_port};
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
+  }
+}
+END
+
+ln -s /etc/nginx/sites-available/${instancia_add}-frontend /etc/nginx/sites-enabled
+EOF
+
+sudo su - root << EOF
+
+cat > /etc/nginx/sites-available/${instancia_add}-phpmyadmin << 'END'
 server {
   server_name $frontend_hostname;
 
   location / {
-    proxy_pass http://127.0.0.1:${frontend_port};
+    proxy_pass http://127.0.0.1:${phpmyadmin_port};
     proxy_http_version 1.1;
     proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection 'upgrade';
