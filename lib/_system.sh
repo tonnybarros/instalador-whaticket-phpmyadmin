d8e6b790497f0dba6e192313d227d004c71178ab
Tonny Barros <68646516+tonnybarros@users.noreply.github.com>
2022-06-20 19:38:08 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -36,7 +36,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone ${link_git}  /home/deploy/${instancia_add}/
+  git clone https://github.com/rtenorioh/Press-Ticket  /home/deploy/${instancia_add}/
 EOF
 
   sleep 2
