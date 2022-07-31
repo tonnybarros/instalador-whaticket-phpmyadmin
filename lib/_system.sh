9682c756ef879d12cda9f259e69dcfdd8a0710ca
Tonny Barros <68646516+tonnybarros@users.noreply.github.com>
2022-07-30 22:24:39 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -74,7 +74,7 @@ system_node_install() {
   sleep 2
 
   sudo su - root <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
   apt-get install -y nodejs
   sleep 2
   npm install -g npm@latest
